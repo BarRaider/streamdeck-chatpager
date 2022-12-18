61f2dd88bfdefe762ed6be703cc6f53b1bd6697e
BarRaider <46548278+BarRaider@users.noreply.github.com>
2022-12-19 01:11:47 +0200

    Support new Ban/Timeout API

---
+++ streamdeck-chatpager/PropertyInspector/BanTimeout.js
@@ -0,0 +1,34 @@
+﻿document.addEventListener('websocketCreate', function () {
+    console.log("Websocket created!");
+    checkSettings(actionInfo.payload.settings);
+
+    websocket.addEventListener('message', function (event) {
+        console.log("Got message event!");
+
+        // Received message from Stream Deck
+        var jsonObj = JSON.parse(event.data);
+
+        if (jsonObj.event === 'didReceiveSettings') {
+            var payload = jsonObj.payload;
+            checkSettings(payload.settings);
+        }
+    });
+});
+
+function checkSettings(payload) {
+    console.log("Checking Settings");
+
+    setDurationSetting("none");
+    if (payload['command'] == 2) {
+        setDurationSetting("");
+    }
+    else {
+        console.log(payload['command'], payload['command'] == 2, payload['command'] === 2);
+    }
+
+}
+
+function setDurationSetting(displayValue) {
+    var dvDuration = document.getElementById('dvDuration');
+    dvDuration.style.display = displayValue;
+}
