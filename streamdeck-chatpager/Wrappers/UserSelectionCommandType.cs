324ff143ebfc19fe394808439d1711552aa5c34c
BarRaider <46548278+BarRaider@users.noreply.github.com>
2022-12-19 01:12:16 +0200

    Refactor to use UserSelection instead of ChatMessage

---
+++ streamdeck-chatpager/Wrappers/UserSelectionCommandType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatPager.Wrappers
+{
+    internal enum UserSelectionEventType
+    {
+        ChatMessage = 0,
+        ApiCommand = 1,
+    }
+}
